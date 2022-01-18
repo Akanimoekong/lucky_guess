dc9e86f9566bf1fdb0cdbe5872b9b9f7ddcea80d
Akanimoekong <akanimoekong@gmail.com>
2022-01-18 20:59:19 +0100

    Splash Screen Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,12 @@
 // import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
-import 'luckdice.dart';
+import 'package:lucky_guess/splashscreen.dart';
 
 void main() {
   return runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Lucky Guess'),
-          backgroundColor: Colors.blue,
-        ),
-        body: const GuessPage(),
-      ),
+      home: MySplashScreen()
     ),
   );
 }
---
+++ lib/splashscreen.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:lucky_guess/luckdice.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => GuessPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // AssetImage(assetName)
+              Image.asset('images/diceapp.gif',
+                height: 150,
+              ),
+              CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Colors.white),
+              )
+            ],
+          )
+
+              //   SplashScreen(
+              //   seconds: 3,
+              //   navigateAfterSeconds: GuessPage(),
+              //   backgroundColor: Colors.blue.shade900,
+              //   title: Text('Lucky Guess'),
+              //   image: Image.asset('images/diceapp.gif'),
+              //   photoSize: 100.0,
+              // ),
+              ),
+        ),
+      ),
+    );
+  }
+}
