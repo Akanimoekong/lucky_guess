1e7f545cbc8827155a14c46ab0c1a9225302f12f
jackieriel <jonahjacksonj@gmail.com>
2022-01-28 09:53:29 +0100

    FIxed rotation on landscape

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,17 @@
 
 // import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lucky_guess/splashscreen.dart';
 
 void main() {
+  // Stop rotation to landscape
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   return runApp(
-    MaterialApp(
-      home: MySplashScreen()
-    ),
+    MaterialApp(home: MySplashScreen()),
   );
 }
